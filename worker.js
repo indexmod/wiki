b34d81b47399921346e35941d94d92e881c3d640
indexmod <23720911+indexmod@users.noreply.github.com>
2026-04-28 17:19:01 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,15 +2,12 @@ import { indexRoute } from "./engine/routes/index-route.js";
 
 export default {
   async fetch(req, env) {
+
     const url = new URL(req.url);
     const path = url.pathname;
 
     if (path === "/" || path === "/index") {
-      return new Response(await indexRoute(env), {
-        headers: {
-          "Content-Type": "text/html; charset=utf-8"
-        }
-      });
+      return await indexRoute(env);
     }
 
     return env.ASSETS.fetch(req);
