296bfb82e9ee747d9867dc7e8903fec38e2028c1
indexmod <23720911+indexmod@users.noreply.github.com>
2026-04-28 21:42:34 +0300

    Update editor-route.js

--- engine/routes/editor-route.js
+++ engine/routes/editor-route.js
@@ -1,20 +1,14 @@
+import { layout } from "../layouts.js";
+import { renderEditor } from "../renders/editor-render.js";
+
 export async function editorRoute(env) {
-  try {
-    const tpl = await layout(env, "editor");
+  const tpl = await layout(env, "editor");
 
-    console.log("[EDITOR LAYOUT]", tpl);
+  const content = renderEditor();
 
-    const content = renderEditor?.() ?? "<p>NO RENDER</p>";
-
-    return tpl
-      .replaceAll("{{title}}", "Editor")
-      .replaceAll("{{layout}}", "editor")
-      .replaceAll("{{nav}}", `<a href="/" class="ui-link">Back</a>`)
-      .replaceAll("{{content}}", content);
-
-  } catch (e) {
-    console.log("[EDITOR CRASH]", e);
-
-    return new Response("EDITOR ERROR: " + e.stack, { status: 500 });
-  }
+  return tpl
+    .replaceAll("{{title}}", "Editor")
+    .replaceAll("{{layout}}", "editor")
+    .replaceAll("{{nav}}", `<a href="/" class="ui-link">Back</a>`)
+    .replaceAll("{{content}}", content);
 }
