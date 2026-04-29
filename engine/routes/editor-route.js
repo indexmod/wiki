4afd5889452565a6e93938861a002e40afc1c9c8
indexmod <23720911+indexmod@users.noreply.github.com>
2026-04-29 14:42:16 +0300

    Update editor-route.js

--- engine/routes/editor-route.js
+++ engine/routes/editor-route.js
@@ -1,5 +1,13 @@
+import { layout } from "../layouts.js";
+import { renderEditor } from "../renders/editor-render.js";
+
 export async function editorRoute(env) {
-  return new Response("EDITOR ROUTE OK", {
-    headers: { "content-type": "text/plain" }
-  });
+  const tpl = await layout(env, "editor");
+  const content = renderEditor();
+
+  return tpl
+    .replaceAll("{{title}}", "Editor")
+    .replaceAll("{{layout}}", "editor")
+    .replaceAll("{{nav}}", `<a href="/" class="ui-link">Back</a>`)
+    .replaceAll("{{content}}", content);
 }
