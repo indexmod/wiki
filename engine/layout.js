47f8428ff9ac3358cfad9217dbe0a8b2f57fb9f3
indexmod <23720911+indexmod@users.noreply.github.com>
2026-04-30 14:12:12 +0300

    Update layout.js

--- engine/layout.js
+++ engine/layout.js
@@ -1,12 +1,9 @@
 // =========================================================
 // CORE LAYOUT ENGINE
-// ROLE: single HTML shell injector
 // =========================================================
 
-export async function layout() {
-  return "<h1>LAYOUT HIT</h1>";
-}
-
+export async function layout(env, { title, content, layout = "page", nav = "" }) {
+  const res = await env.ASSETS.fetch("/layouts/base.html");
   const html = await res.text();
 
   return html
